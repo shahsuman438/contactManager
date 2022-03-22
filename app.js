49fa66f0724495b281409352bc84d6095c169d5a
suman sah <shahsuman438@gmail.com>
2022-03-23 00:30:14 +0545

    Created user Registration API with auth Token generator

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const mongoose= require('mongoose')
 const app=express();
 const url = 'mongodb://localhost/ContactManager'
-
+const contactRouter=require('./routers/contact')
+const userRouter=require('./routers/user')
 
 
 
@@ -14,12 +15,12 @@ connect.on('open',()=>{
 })
 
 
-const contactRouter=require('./routers/contact')
+
 
 app.use(express.json())
 app.use('/contact',contactRouter)
 app.use('/uploads',express.static('uploads'))
-
+app.use('/auth',userRouter)
 app.listen(4000,()=>{
     console.log("#########SERVER STARTED...########")
 })
---
+++ routers/user.js
@@ -0,0 +1,52 @@
+const express=require('express')
+const userModel=require('../models/user')
+
+const router=express.Router()
+
+
+const jwt=require('jsonwebtoken')
+
+
+router.post('/register',async(req,res)=>{
+    let userdata=req.body
+    let user=new userModel(userdata)
+    user.save((error,registerUser)=>{
+        if(error){
+            console.log('error',error)
+        }else{
+            let payload={subject:registerUser._id}
+            let token=jwt.sign(payload,'sk1443')
+            res.status(200).send({token})
+        }
+    })
+})
+
+router.get('/user/:id',async(req,res)=>{
+    try{
+        userModel.findById(req.params.id).then(
+            (data)=>{
+                res.status(200).send(data)
+            }
+        )
+    }catch(error){
+        res.status(404).send({
+            "msg":"user not found"
+        })
+    }
+})
+
+
+router.get('/user',async(req,res)=>{
+    try{
+        userModel.find().then(data=>{
+            res.status(200).send(data)
+        }).catch(err=>{res.status(404).send({"msg":"user not found"})})
+    }catch(error){
+        res.status(500).send({
+            "msg":"somthing went wrong"
+        })
+    }
+})
+
+module.exports=router
+
